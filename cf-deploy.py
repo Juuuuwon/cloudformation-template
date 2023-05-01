28c1c0a131f067678091f2d76b106f8bfff11a5b
hmoon630 <skyuecx0630@gmail.com>
2023-05-01 17:28:43 +0900

    add a function which describes stacks

--- cf-deploy.py
+++ cf-deploy.py
@@ -2,6 +2,7 @@ from typing import TypedDict, Iterable, Reversible
 from threading import Thread
 from sys import exit
 from time import sleep
+from colorama import Fore, Style
 
 import logging
 import sys
@@ -45,7 +46,7 @@ def delete_stack(stack_name) -> None:
 def get_capabilities(template_body):
     response = client.validate_template(TemplateBody=template_body)
 
-    return response.get("Capabilities")
+    return response.get("Capabilities", [])
 
 
 def wait_stack(operation, stack_name, delay=5, max_attempts=120):
@@ -69,8 +70,7 @@ def build_template_params(stack_name, template_path, parameters: dict):
         ],
     }
 
-    if capabilities := get_capabilities(template_body):
-        param["Capabilities"] = capabilities
+    param["Capabilities"] = get_capabilities(template_body)
 
     return param
 
@@ -92,7 +92,7 @@ def deploy_in_order(stack_params: Iterable[dict], method=create_stack):
             logger.info(f"{param['StackName']} is being {message}")
 
             wait_stack(wait_operation, param["StackName"])
-            logger.info(f"{param['StackName']} is created")
+            logger.info(f"{param['StackName']} is {message}")
     except Exception as e:
         logger.error(e)
 
@@ -107,7 +107,7 @@ def deploy_parallel(stack_sequences: Iterable[Iterable[dict]], method=create_sta
 
         jobs.append(job)
 
-    while filter(lambda job: job.is_alive(), jobs):
+    while list(filter(lambda job: job.is_alive(), jobs)):
         sleep(1)
 
     return stack_sequences
@@ -135,40 +135,97 @@ def delete_parallel(stack_sequences: Iterable[Iterable[dict]]):
         job.start()
         jobs.append(job)
 
-    while filter(lambda job: job.is_alive(), jobs):
+    while list(filter(lambda job: job.is_alive(), jobs)):
         sleep(1)
 
     return stack_sequences
 
 
-if __name__ == "__main__":
-    codecommit_template_path = "C:\\skills\\template\\ecr-pipeline\\codecommit.yaml"
-    codebuild_template_path = "C:\\skills\\template\\ecr-pipeline\\codebuild.yaml"
-    codepipeline_template_path = "C:\\skills\\template\\ecr-pipeline\\codepipeline.yaml"
+def describe_sequence(stack_sequences: Iterable[Iterable[dict]]):
+    for num, stacks in enumerate(stack_sequences, start=1):
+        stack_infos = [
+            {
+                "StackName": stack["StackName"],
+                "Capabilities": stack["Capabilities"],
+                "Parameters": stack["Parameters"],
+            }
+            for stack in stacks
+        ]
+        print(f"{Fore.WHITE}===== Stack sequence {num} =====")
 
-    apps = ["account", "token", "event", "status", "stress"]
+        for info in stack_infos:
+            print(
+                f"{Fore.LIGHTGREEN_EX}{info['StackName']}"
+                if info["Capabilities"]
+                else f"{Fore.LIGHTGREEN_EX}{Style.BRIGHT}{info['StackName']}\t{info['Capabilities']}{Style.RESET_ALL}"
+            )
+            parameters = [
+                f"{Fore.LIGHTBLACK_EX}{param['ParameterKey']}: {Fore.WHITE}{Style.BRIGHT}{param['ParameterValue']}{Style.RESET_ALL}"
+                for param in info["Parameters"]
+            ]
+            print("\t" + "\n\t".join(parameters))
+
+        print(f"{Fore.RESET}{Style.RESET_ALL}")
+    return
+
+def check_user_admission():
+    retry = 0
+    while retry < 2:
+        admission = input("Coninue with those stacks? (y / N): ").lower()
+        if admission == "n":
+            break
+        elif admission == "y":
+            return
+        else:
+            print("Please enter 'y' or 'n'")
+            retry += 1
+    print("Cancelled")
+    exit(0)
+
+
+if __name__ == "__main__":
+    codecommit_template_path = "C:\\skills\\template\\blue-green\\codecommit.yaml"
+    codebuild_template_path = "C:\\skills\\template\\blue-green\\codebuild.yaml"
+    codedeploy_template_path = "C:\\skills\\template\\blue-green\\codedeploy.yaml"
+    codepipeline_template_path = "C:\\skills\\template\\blue-green\\codepipeline.yaml"
+
+    apps = ["customer", "order", "product", "stress"]
     ARTIFACT_BUCKET = "wsi-hmoon-artifacts"
     CODECOMMIT_BRANCH = "main"
 
     codecommit_stack_params = [
         build_template_params(
-            stack_name=f"{REGION}-{app_name}-git",
+            stack_name=f"dev-{app_name}-git",
             template_path=codecommit_template_path,
-            parameters={"RepositoryName": f"{REGION}-{app_name}"},
+            parameters={"RepositoryName": f"dev-{app_name}"},
         )
         for app_name in apps
     ]
 
     codebuild_stack_params = [
         build_template_params(
-            stack_name=f"{REGION}-{app_name}-build",
+            stack_name=f"dev-{app_name}-build",
             template_path=codebuild_template_path,
             parameters={
-                "ProjectName": f"{REGION}-{app_name}-build",
-                "CodeCommitRepositoryName": f"{REGION}-{app_name}",
+                "CodeBuildProjectName": f"dev-{app_name}-build",
+                "CodeCommitRepositoryName": f"dev-{app_name}",
                 "CodeCommitBranchName": CODECOMMIT_BRANCH,
                 "ArtifactBucketName": ARTIFACT_BUCKET,
                 "ECRRepositoryName": app_name,
+            },
+        )
+        for app_name in apps
+    ]
+
+    codedeploy_stack_params = [
+        build_template_params(
+            stack_name=f"dev-{app_name}-deploy",
+            template_path=codedeploy_template_path,
+            parameters={
+                "CodeDeployApplicationName": f"dev-{app_name}-deploy",
+                "ECSServiceName": app_name,
+                "TargetGroup1Name": f"{app_name}-1",
+                "TargetGroup2Name": f"{app_name}-2",
             },
         )
         for app_name in apps
@@ -179,10 +236,12 @@ if __name__ == "__main__":
             stack_name=f"{REGION}-{app_name}-pipeilne",
             template_path=codepipeline_template_path,
             parameters={
-                "CodePipelineName": f"{REGION}-{app_name}-pipeline",
-                "CodeCommitRepositoryName": f"{REGION}-{app_name}",
+                "CodePipelineName": f"dev-{app_name}-pipeline",
+                "CodeCommitRepositoryName": f"dev-{app_name}",
                 "CodeCommitBranchName": CODECOMMIT_BRANCH,
-                "CodeBuildProjectName": f"{REGION}-{app_name}-build",
+                "CodeBuildProjectName": f"dev-{app_name}-build",
+                "CodeDeployApplicationName": f"dev-{app_name}-deploy",
+                "CodeDeployDeploymentGroupName": f"wsi-cluster-{app_name}",
                 "ArtifactBucketName": ARTIFACT_BUCKET,
             },
         )
@@ -195,11 +254,17 @@ if __name__ == "__main__":
             zip(
                 codecommit_stack_params,
                 codebuild_stack_params,
+                codedeploy_stack_params,
                 codepipeline_stack_params,
             )
         )
 
-        deploy_parallel(sequences, method=create_stack)
+        # Inform a user what are going to be executed
+        describe_sequence(sequences)
+        check_user_admission()
 
+        # Execute
+        delete_parallel(sequences)
+        # deploy_parallel(sequences, method=update_stack)
     except Exception as e:
         logger.error(e)
