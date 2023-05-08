97a8be600e06e84c9d697213200ce305e47db61e
hmoon630 <skyuecx0630@gmail.com>
2023-05-08 21:52:43 +0900

    install pypy3 if not exists

--- codepipeline/ec2/in-place/scripts/before_install.sh
+++ codepipeline/ec2/in-place/scripts/before_install.sh
@@ -2,3 +2,10 @@
 if [ ! -d /opt/app ]; then
     mkdir -p /opt/app
 fi
+
+if [ ! -f /usr/bin/pypy3 ]; then
+    wget https://downloads.python.org/pypy/pypy3.9-v7.3.11-linux64.tar.bz2 -P /tmp/
+    tar xf /tmp/pypy3.9-v7.3.11-linux64.tar.bz2 -C /tmp/
+    ln -s /tmp/pypy3.9-v7.3.11-linux64/bin/pypy3 /usr/bin/pypy3
+    pypy3 -m ensurepip
+fi
