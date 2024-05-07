3124a54b37f2afc872749039d8973599af4814bd
hmoon630 <skyuecx0630@gmail.com>
2024-05-07 10:08:53 +0900

    make scripts work on a clean AMI

--- codepipeline/ec2/scripts/after_install.sh
+++ codepipeline/ec2/scripts/after_install.sh
@@ -1,12 +1,29 @@
 #!/bin/bash -eux
 
-pip3 install -r /opt/app/requirements.txt
+# If app.service exists, disable and stop it
+if systemctl list-units | grep app.service
+then
+    systemctl disable --now app.service
+fi
 
+# If venv does not exist
+if [ ! -d /opt/app/venv ]
+then
+    python3 -m ensurepip
+    python3 -m pip install virtualenv
+    python3 -m virtualenv /opt/app/venv
+fi
+
+# Install python packages
+/opt/app/venv/bin/pip3 install -r /opt/app/requirements.txt
+
+# Delete application __pycache__
 if [ -d /opt/app/__pycache__ ]
 then
     rm -rf /opt/app/__pycache__
 fi
 
+# Overwrite app.service
 if [ -e /opt/app/app.service ]
 then
     cp /opt/app/app.service /etc/systemd/system/app.service
--- codepipeline/ec2/scripts/before_install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -eux
-
-if systemctl list-units | grep app.service
-then
-    systemctl disable --now app.service
-fi
